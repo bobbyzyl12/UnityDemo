f7f36a62627f078ad548ddad4e36bbd93df1bc8c
bobbyzyl12 <bobbyzyl12@126.com>
2017-06-25 20:31:19 +0800

    button demo

---
+++ Assets/ClickScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ClickScript : MonoBehaviour {
+
+    public void Clicked() {
+        Debug.Log("clicked");
+        string passwordInput = GameObject.Find("LonInput").GetComponent<InputField>().text;
+        Debug.Log(passwordInput);
+    }
+}

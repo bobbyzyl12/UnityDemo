a812f068faf4af19a1489ae5d4bf758e1742dc03
bobbyzyl12 <bobbyzyl12@126.com>
2017-06-25 20:34:15 +0800

    hide game object

---
+++ Assets/HideObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideObject : MonoBehaviour {
+    GameObject gb;
+    void Start()
+    {
+        gb = GameObject.Find("Cube");
+    }
+
+    public void hideButtonClicked() {
+        if (gb.active == true)
+        {
+            gb.SetActive(false);
+
+        }
+        else if (gb.active == false)
+        {
+            gb.SetActive(true);
+        }
+    }
+}
